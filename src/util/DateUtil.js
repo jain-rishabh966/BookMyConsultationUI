618e7288e83ffc047f81bb97024f93c37778578f
Rishabh <jain.rishabh966@gmail.com>
2021-09-29 01:42:50 +0530

    Use of moment for date string

--- src/util/DateUtil.js
+++ src/util/DateUtil.js
@@ -2,13 +2,6 @@ module.exports = {
     getDateString: date => {
         date = new Date(date);
 
-        let day = date.getDate();
-        let month = date.getMonth();
-        let year = date.getFullYear();
-
-        if (day <= 9) day = '0' + day;
-        if (month <= 9) month = '0' + month;
-
-        return year + '-' + month + '-' + day;
+        return require('moment')(date).format('YYYY-MM-DD');
     }
 };
